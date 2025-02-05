6c0520d993adeb9f11a620a22b0a51a6caf12275
shahzodotaboyev <shoxzodbek15@gmail.com>
2025-02-05 22:37:44 +0500

    message user

--- app.js
+++ app.js
@@ -3,28 +3,52 @@ const messageInput = document.querySelector(".message-input");
 const sendMessagebutton = document.querySelector("#send-message");
 
 const userData = {
-    message:null
-}
-const createMessageElement = (content, classes) => {
+  message: null,
+};
+const createMessageElement = (content, ...classes) => {
   const div = document.createElement("div");
-  div.classList.add("message", classes);
+  div.classList.add("message", ...classes);
   div.innerHTML = content;
   return div;
 };
 
 const handleOutgoingMessage = (e) => {
   e.preventDefault();
-
-  userData.message = messageInput.value.trim()
-
+  userData.message = messageInput.value.trim();
+  messageInput.value = "";
   const messageContent = `  <div class="message-text"></div>`;
   const outgoingMessagediv = createMessageElement(
     messageContent,
-    "user-message" 
+    "user-message"
   );
-  outgoingMessagediv.querySelector('.message-text').textContent = userData.message
+  outgoingMessagediv.querySelector(".message-text").textContent =
+    userData.message;
 
   chatBody.appendChild(outgoingMessagediv);
+  setTimeout(() => {
+    const messageContent = ` <img
+            class="bot-avatar"
+            width="45px"
+            height="45px"
+            src="./img/image (1).png"
+            alt=""
+          />
+          <div class="message-text">
+            <div class="thinking-indicator">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+            </div>
+          </div>`;
+    const incomingMessagediv = createMessageElement(
+      messageContent,
+      "bot-message","thinking"
+    );
+    
+    
+
+    chatBody.appendChild(incomingMessagediv);
+  }, 600);
 };
 
 messageInput.addEventListener("keydown", (e) => {
